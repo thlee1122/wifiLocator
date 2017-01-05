ea87bc8f4f4b42b83d6568a475ad9932a08d76f1
Tae Hoon Lee <thlee1122@hotmail.com>
2017-01-05 14:16:54 -0500

    heroku

--- server.js
+++ server.js
@@ -7,29 +7,13 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
 
-// app.use(function(req, res, next) {
-// 	if(req.headers['x-forwarded-proto'] === 'http') {
-// 		res.redirect('http://' + req.hostname + req.url);
-// 	} else {
-// 		next();
-// 	}
-// });
-
-
-var forceSsl = function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    }
-    return next();
- };
-
-app.configure(function () {
-
-    if (env === 'production') {
-        app.use(forceSsl);
-    }
-    // other configurations etc for express go here...
-}
+app.use(function(req, res, next) {
+	if(req.headers['x-forwarded-proto'] === 'http') {
+		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
+	}
+});
 
 app.get('/', function(req, res, next) {
 	res.sendFile('index.html')
