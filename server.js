8075457a8b7812d722e281d45ada8e7e18f710ca
Tae Hoon Lee <thlee1122@hotmail.com>
2017-01-05 13:48:04 -0500

    Final Version

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 
-
 var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
