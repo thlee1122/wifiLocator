61b9d0396c9d50b25fa9e39ca79081f5628b9ea3
Tae Hoon Lee <thlee1122@hotmail.com>
2017-01-05 14:05:02 -0500

    rr

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 
 
 app.use(function(req, res, next) {
-	if(req.headers['x-forwarded-proto'] === 'http') {
+	if(req.headers['x-forwarded-proto'] === 'https') {
 		res.redirect('http://' + req.hostname + req.url);
 	} else {
 		next();
