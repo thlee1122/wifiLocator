314472d638d43e8252b895ba3709cb4fa52358c2
Tae Hoon Lee <thlee1122@hotmail.com>
2017-01-05 14:01:09 -0500

    ss

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var path = require("path");
 var bodyParser = require("body-parser");
 
 var app = express();
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
+app.get('/', function(req, res, next) {
+	res.sendFile('index.html')
+});
+
   // Initialize the app.
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3001, function () {
   console.log("App now running on port");
 });
