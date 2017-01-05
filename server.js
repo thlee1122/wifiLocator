4551c09abb13801cacaf210ea103807c9c4de7db
Tae Hoon Lee <thlee1122@hotmail.com>
2017-01-05 11:34:04 -0500

    Final Version

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.listen(PORT, function() {
 	console.log('Express server is up on part ' + PORT);
