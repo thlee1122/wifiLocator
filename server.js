1332626d51665d99d48328f153351e923832da56
Tae Hoon Lee <thlee1122@hotmail.com>
2017-01-05 14:15:22 -0500

    ee

--- server.js
+++ server.js
@@ -7,13 +7,29 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
 
-app.use(function(req, res, next) {
-	if(req.headers['x-forwarded-proto'] === 'http') {
-		res.redirect('https://' + req.hostname + req.url);
-	} else {
-		next();
-	}
-});
+// app.use(function(req, res, next) {
+// 	if(req.headers['x-forwarded-proto'] === 'http') {
+// 		res.redirect('http://' + req.hostname + req.url);
+// 	} else {
+// 		next();
+// 	}
+// });
+
+
+var forceSsl = function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    return next();
+ };
+
+app.configure(function () {
+
+    if (env === 'production') {
+        app.use(forceSsl);
+    }
+    // other configurations etc for express go here...
+}
 
 app.get('/', function(req, res, next) {
 	res.sendFile('index.html')
