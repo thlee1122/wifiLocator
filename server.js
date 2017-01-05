fd5e1cd92bee74479c29479ebbc58661b13f992b
Tae Hoon Lee <thlee1122@hotmail.com>
2017-01-05 14:24:24 -0500

    ee

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ app.use(bodyParser.json());
 // 	}
 // });
 
+app.use(forceSsl(req, res, next) {
+	if(req.headers['x-forwarded-proto'] != 'https') {
+		res.redirect("https://#{req.header 'host'}#{req.url}");
+	} else {
+		next();
+	}
+});
+
+
 app.configure 'production', ->
   app.use forceSsl(req, res, next) ->
     if req.header 'x-forwarded-proto' != 'https'
